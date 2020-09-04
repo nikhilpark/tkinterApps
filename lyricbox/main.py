1bbd43835131e0df04dfc9456d2a9e9a11e6e88f
Nikhil <nikhil.speedy@gmail.com>
2020-09-04 14:41:22 +0530

    Update main.py

--- lyricbox/main.py
+++ lyricbox/main.py
@@ -1,10 +1,10 @@
-from tkinter import Tk, Label, Frame, BOTH, Canvas, LEFT, VERTICAL, RIGHT, Y, Entry, Button
-from tkinter import ttk
+from tkinter import Tk, ttk, Label, Frame, BOTH, Canvas, LEFT, VERTICAL, RIGHT, Y, Entry, Button
 from io import BytesIO
 from PIL import ImageTk, Image
 from bs4 import BeautifulSoup
 import requests
-global albumartimg
+import threading
+global albumartimg, artist, title, albumname, lyrics
 
 root = Tk()
 root.title('Lyric Box')
@@ -12,33 +12,38 @@ root.iconbitmap(r"images\songwriter.ico")
 root.geometry("450x600")
 
 
-# button command
 
 
-def getlyrics():
-    global albumartimg
+def getinput():
 
-    artist = artistName.get()
+    global title
+    global artist
+
     title = songName.get()
-    url = r"https://api.lyrics.ovh/v1/" + artist + "/" + title
+    artist = artistName.get()
 
-    response = requests.get(url)
-    lyrics = str(response.content)
-    lyrics = (lyrics[13:-3])
-    lyrics = lyrics.replace(r"\\n", "\n").replace("\\r", "")
-    lyrics = lyrics.replace("\\", "").replace("xe2x80x85", "").replace("xc2x85", "")
+
+def getalbumname():
+
+    global albumname
 
     urlalbum = requests.get(
-        "https://www.azlyrics.com/lyrics/" + artist.lower().replace(" ", "")
-        + "/" + title.lower().replace(" ", "") + ".html",
-        timeout=10
+       "https://www.last.fm/music/" + artist.replace(" ", "+")
+       + "/_/" + title.replace(" ", "+") + "/+albums",
+       timeout=10
     )
 
     srcc = urlalbum.content
     soupp = BeautifulSoup(srcc, 'lxml')
-    albumname = soupp.find_all("div", {"class": "songinalbum_title"})[0]
-    albumname = (str(albumname)[42:-18]).replace("&amp;", "&")
+    albumname = soupp.find_all("a", {"class": "link-block-target"})[0]
 
+    albumname = str(albumname)
+    loc = albumname.find("url") + 5
+    albumname = (albumname[loc:-4])
+
+
+def getalbumart():
+    global albumartimg
     urlalbumart = requests.get(
         "https://www.last.fm/music/" + artist.replace(" ", "+")
         + "/" + albumname.replace(" ", "+") + "/+images",
@@ -54,10 +59,29 @@ def getlyrics():
     img_data = imgresponse.content
 
     albumartimg = ImageTk.PhotoImage(Image.open(BytesIO(img_data)))
+
+
+def getlyrics():
+
+    global lyrics
+
+    url = r"https://api.lyrics.ovh/v1/" + artist + "/" + title
+
+    response = requests.get(url)
+    lyrics = str(response.content)
+    lyrics = (lyrics[13:-3])
+    lyrics = lyrics.replace(r"\\n", "\n").replace("\\r", "")
+    lyrics = lyrics.replace("\\", "").replace("xe2x80x85", "").replace("xc2x85", "")
+
+
+def printstuff():
+
     artlabel = Label(second_frame, image=albumartimg)
     artlabel.grid(row=0, column=0, pady=(20, 10))
 
-    mylabel3 = Label(second_frame, text="LYRICS :")
+    mylabel3 = Label(
+        second_frame, text=artist.capitalize() + " - " + title.capitalize() + " (" + albumname + ") " + "\n\nLYRICS :"
+    )
     mylabel3.grid(row=8, column=0)
 
     mylabel4 = Label(second_frame, text="------------------------")
@@ -68,6 +92,44 @@ def getlyrics():
 
     mylabel6 = Label(second_frame, text="------------------------")
     mylabel6.grid(row=11, column=0, pady=10)
+
+
+def buttonaction():
+
+    message = "Not found! try different input"
+
+    mylabel7 = Label(second_frame, text="Processing... ")
+    mylabel7.grid(row=8, column=0)
+
+    getinput()
+    getalbumname()
+    try:
+        getalbumname()
+        
+    except IndexError:
+        print("Error grabbing albumname")
+        mylabel8 = Label(second_frame, text=message)
+        mylabel8.grid(row=8, column=0)
+        exit()
+
+    try:
+        getalbumart()
+    except IndexError:
+        print("Error grabbing albumart")
+
+        mylabel8 = Label(second_frame, text=message)
+        mylabel8.grid(row=8, column=0)
+        exit()
+
+    try:
+        getlyrics()
+    except IndexError:
+        print("Error grabbing lyrics")
+        mylabel8 = Label(second_frame, text=message)
+        mylabel8.grid(row=8, column=0)
+        exit()
+
+    printstuff()
 
 
 '''
@@ -105,7 +167,7 @@ artistName = Entry(second_frame, width=50, borderwidth=5)
 
 
 # BUTTON
-go_Btn = Button(second_frame, text="Go!", command=getlyrics)
+go_Btn = Button(second_frame, text="Go!", command=lambda: threading.Thread(target=buttonaction).start())
 
 # GRID
 imageLabel.grid(row=0, column=0, pady=(20, 10))
